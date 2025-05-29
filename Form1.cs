5c33e717e732dd60d1a596ac3542ffa183e2171f
minisv <minisv@naver.com>
2025-05-30 07:12:55 +0900

    Init : init

---
+++ Form1.cs
@@ -0,0 +1,14 @@
+namespace CHEF.net;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void label1_Click(object sender, EventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
